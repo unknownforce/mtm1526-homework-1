b96843aea9f3ebe1b835803069dff89ce5d47efb
Petrus Chan <petchan34@gmail.com>
2012-01-15 23:23:24 -0500

    updated javascript file

--- js/functions.js
+++ js/functions.js
@@ -17,9 +17,62 @@ console.log(letter);
 
 
 function pickLetter () {
-	for (var i = 0; i <= 9; i++) {	
-		document.write(letter + "<br>");
+	document.write(letter);
+	document.write('<br>');
+	
+	for (var i = 0; i <= 1; i++) {	
+		document.write(letter);
 	}
+	
+	document.write('<br>');
+	
+	for (var i = 0; i <= 2; i++) {	
+		document.write(letter);
+	}
+	
+	document.write('<br>');
+	
+	for (var i = 0; i <= 3; i++) {	
+		document.write(letter);
+	}
+	
+	document.write('<br>');
+	
+	for (var i = 0; i <= 4; i++) {	
+		document.write(letter);
+	}
+	
+	document.write('<br>');
+	
+	for (var i = 0; i <= 5; i++) {	
+		document.write(letter);
+	}
+	
+	document.write('<br>');
+	
+	for (var i = 0; i <= 6; i++) {	
+		document.write(letter);
+	}
+	
+	document.write('<br>');
+	
+	for (var i = 0; i <= 7; i++) {	
+		document.write(letter);
+	}
+	
+	document.write('<br>');
+	
+	
+	for (var i = 0; i <= 8; i++) {	
+		document.write(letter);
+	}
+	
+	document.write('<br>');
+	
+	for (var i = 0; i <= 9; i++) {	
+		document.write(letter);
+	}
+	
 }
 
 pickLetter();
